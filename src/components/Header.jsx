2014e4e3ebecb0dd31930034e113aaf46440eb27
Firoz <146817644+Firozfs5@users.noreply.github.com>
2025-10-18 23:10:22 +0530

    navbar ui change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,18 +116,3 @@ const Header = () => {
 };
 
 export default Header;
-{
-  /* <img
-              className="w-12 h-12 mr-2 mx-5 rounded"
-              src={user.photoURL}
-              alt=""
-            /> */
-}
-{
-  /* <button
-              onClick={() => handleSignOut()}
-              className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-            >
-              Sign Out
-            </button> */
-}
